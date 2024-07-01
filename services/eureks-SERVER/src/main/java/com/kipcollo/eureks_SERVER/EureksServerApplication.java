4ccaef9af38e9f3d6089f952e446eaf1d90511c4
Collins Kipkosgei <kipkosgei.tech@gmail.com>
2024-07-01 18:07:49 +0300

    Implemented configuration server and added discovery - eureka server

--- services/eureks-SERVER/src/main/java/com/kipcollo/eureks_SERVER/EureksServerApplication.java
+++ services/eureks-SERVER/src/main/java/com/kipcollo/eureks_SERVER/EureksServerApplication.java
@@ -2,8 +2,10 @@ package com.kipcollo.eureks_SERVER;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EureksServerApplication {
 
 	public static void main(String[] args) {
